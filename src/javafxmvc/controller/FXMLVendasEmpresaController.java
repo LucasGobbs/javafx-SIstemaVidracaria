6030642ecc28dbbca27b53409c334f03c3c2f8d3
LucasGobbs <llcostagobbi@gmail.com>
2020-08-08 02:44:05 -0300

    Initial commit

---
+++ src/javafxmvc/controller/FXMLVendasEmpresaController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxmvc.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author joana
+ */
+public class FXMLVendasEmpresaController implements Initializable {
+
+    @FXML
+    private MenuItem menuItemCadastrosClientes;
+    @FXML
+    private MenuItem menuItemProcessosVendas;
+    @FXML
+    private MenuItem menuItemGraficosVendasPorMes;
+    @FXML
+    private MenuItem menuItemRelatoriosQuantidadeProdutos;
+    @FXML
+    private AnchorPane anchorPane;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
